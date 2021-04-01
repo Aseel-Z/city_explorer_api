0d2c5334a1e9951fbe25d8372c3dc8836f6ec89a
Aseel-Z <aseelalzweri@gmail.com>
2021-04-01 22:27:18 +0300

    lab09---

--- server.js
+++ server.js
@@ -1,144 +1,264 @@
 'use strict';
-
+// dotev
 require('dotenv').config();
-const PORT = process.env.PORT || 3000
+
+// port
+
+const PORT = process.env.PORT || 3000;
+const ENV = process.env.ENV;
+
+// Depedencies
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
 const superagent = require('superagent');
 
-const DATABASE_URL = process.env.DATABASE_URL;
-const cityApp = express();
-
-const client = new pg.Client(DATABASE_URL);
-client.connect();
-
+// API Keys & Database URLs
 const GEO_CODE_API_KEY = process.env.GEO_CODE_API_KEY;
+const DATABASE_URL = process.env.DATABASE_URL;
+const WEATHER_CODE_API_KEY = process.env.WEATHER_CODE_API_KEY;
+const PARK_CODE_API_KEY = process.env.PARK_CODE_API_KEY;
 const MOVIE_API_KEY =  process.env.MOVIE_API_KEY;
 const YELP_API_KEY = process.env.YELP_API_KEY;
+
+// express wireframe - app launch
+const cityApp = express();
 cityApp.use(cors());
 
+// pg = node-postgres (a collection of node.js modules for interfacing with your PostgreSQL database system)
+// if it was on local it will return a value for ENV which is a variable I added to .env that will be in local and not in ureko because of get ignore
+let client = '';
+if (ENV === 'DEV') {
+  client = new pg.Client({
+    connectionString: DATABASE_URL
+  });
+} else {
+  client = new pg.Client({
+    connectionString: DATABASE_URL,
+    ssl: {}
+  });
+}
+
+// connect app to the postgres database
+client.connect();
+
 // Path creation
-cityApp.get('/location', handleLocationReq);
+// cityApp.get('/location', handleLocationReq);
+cityApp.get('/weather', handleWeatherReq);
+cityApp.get('/parks', handleParksReq);
 cityApp.get('/movies', handleMoviesReq);
-cityApp.get('/yelpmovies', handleYelpReq);
+// cityApp.get('/yelpmovies', handleYelpReq);
+
+// // Location Request Handler Function + Constructor Function
+// function Location(searchQuery, dataLocation) {
+//   this.tableName = 'locations'
+//   this.search_query = searchQuery;
+//   this.formatted_query = dataLocation.display_name;
+//   this.longitude = dataLocation.lon;
+//   this.latitude = dataLocation.lat;
+// }
+
+// function getDataFromAPI() {
+//   const url = `https://us1.locationiq.com/v1/search.php`;
+//   const locationQueryPara = {
+//     key: GEO_CODE_API_KEY,
+//     city: searchQuery,
+//     format: 'json',
+//   };
+//   superagent
+//     .get(url)
+//     .query(locationQueryPara)
+//     .then((locationData) => {
+//       const newLocation = new Location(searchQuery, locationData.body[0]);
+//       // send data sent through API to database
+//       const sqlQuery = `INSERT INTO locations(search_query, formatted_query, longitude, latitude) VALUES( $1, $2, $3, $4)`;
+//       const values = [newLocation.search_query, newLocation.formatted_query, newLocation.longitude, newLocation.latitude];
+//       client.query(sqlQuery, values);
+//       res.status(200).send(newLocation);
+//     })
+// };
 
 
-function handleLocationReq(req, res) {
-  // client input 
-  const searchQuery = req.query.city;
+// function handleLocationReq(req, res) {
+//   try {
+//     // client input 
+//     const searchQuery = req.query.city;
 
-  // error message if there is no input
-  if (!searchQuery) {
-    res.status(500).send('Sorry, something went wrong');
-  };
+//     // error message if there is no input
+//     if (!searchQuery) {
+//       res.status(500).send('Sorry, something went wrong');
+//     };
 
-  // if there is input get the data from the database 
-  const values = [searchQuery]
-  const sqlQuery = `SELECT * FROM locations WHERE search_query=$1`
+//     // if there is input get the data from the database in it is already there or get it from API and then insert it into database
+//     // get the data from database where the value
 
-  // then check if it is in the database (client that is connected) and pass the query (sql)
-  client.query(sqlQuery,values).then( table => {
-   if (table.rows.length === 0) {
-    throw error;
-   }
-  res.status(200).json(table.row[0])}).
-  catch((error) => {
-    // URL API
-    const url = `https://us1.locationiq.com/v1/search.php` 
-    const locationQueryPara = {
-      key: GEO_CODE_API_KEY,
-      city: searchQuery,
+//     const values = [searchQuery]
+//     const sqlQuery = `SELECT * FROM locations WHERE search_query=$1`
+
+//     // then check if it is in the database (client that is connected) and pass the query (sql)
+//     client.query(sqlQuery, values).then(tableResult => {
+
+//       if (tableResult.rows.length === 0) {
+//         getDataFromAPI();
+//       } else {
+//         res.status(200).json(table.row[0])
+//       }
+//     })
+//   }
+//   catch (error) {
+//     res.status(500).send('Sorry, something went wrong')
+//   }
+// }
+
+// Weather Request Handler Function + Constructor Function 
+
+function Weather(data) {
+  this.forecast = data.weather.description;
+  this.time = data.valid_date
+}
+
+function handleWeatherReq(req, res) {
+  try {
+    const searchQueryLat = req.query.lat;
+    const searchQueryLon = req.query.lon;
+    if (!searchQueryLat || !searchQueryLon) {
+      res.status('500').send('Sorry, something went wrong');
+    }
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily`;
+    const weatherQueryPara = {
+      key: WEATHER_CODE_API_KEY,
+      lat: searchQueryLat,
+      lon: searchQueryLon,
       format: 'json',
     };
-  superagent
-    .get(url)
-    .query(locationQueryPara)
-    .then((locationData) => {
-      const newLocation = new Location(searchQuery, locationData.body[0]);
-      // send data sent through API to database
-      const sqlQuery = `INSERT INTO locations(search_query, formatted_query, longitude, latitude) VALUES( $1, $2, $3, $4)`;
-      const values = [newLocation.search_query, newLocation.formatted_query, newLocation.longitude, newLocation.latitude ];
-      client.query(sqlQuery,values);    
-      res.status(200).send(newLocation);
-    })
-    .catch((error) =>
-      res.status(500).send('Sorry, something went wrong'))
-})}
 
-function Location(searchQuery, dataLocation) {
-  this.tableName = 'locations'
-  this.search_query = searchQuery;
-  this.formatted_query = dataLocation.display_name;
-  this.longitude = dataLocation.lon;
-  this.latitude = dataLocation.lat;
+    superagent
+      .get(url)
+      .query(weatherQueryPara)
+      .then((weatherData) => {
+        const dailyWeather = weatherData.body.data.map((weather) => {
+          return new Weather(weather);
+        });
+        res.status(200).send(dailyWeather.slice(0, 9));
+      });
+
+  } catch (error) {
+    res.status(500).send('internal weather server error occured');
+  }
 }
-  
 
-  
+// Parks Request Handler Function + Constructor Function
+
+function Park(parkData) {
+  this.name = parkData.fullName;
+  this.description = parkData.description;
+  this.address = `${parkData.addresses[0].line1}${parkData.addresses[0].city}${parkData.addresses[0].stateCode}${parkData.addresses[0].postalCode}`
+  this.fees = parkData.fees[0] || '0.00';
+  this.url = parkData.url;
+}
+
+function handleParksReq(req, res) {
+  try {
+    const searchQuery = req.query.location;
+    if (!searchQuery) {
+      res.status('500').send('Sorry, something went wrong');
+    }
+    const url = `https://developer.nps.gov/api/v1/parks`
+    const parkQueryPara = {
+      api_key: PARK_CODE_API_KEY,
+      location: searchQuery,
+      limit: '10'
+    };
+    superagent
+      .get(url)
+      .query(parkQueryPara)
+      .then((parkData) => {
+        const parks = parkData.body.data.map((park) => {
+          return new Park(park);
+        });
+        res.status(200).send(parks);
+      });
+  } catch (error) {
+    res.status(500).send('internal parks server error occured');
+  }
+}
+
+// Movies Request Handler Function + Constructor Function
+
+function Movie(dataMovie) {
+  this.title = dataMovie.title ;
+  this.overview = dataMovie.overview;
+  this.average_votes = dataMovie.vote_average;
+  this.total_votes = dataMovie.vote_count;
+  this.image_url = dataMovie.poster_path;
+  this.popularity = dataMovie.popularity;
+  this.released_on = dataMovie.release_date;
+}
 function handleMoviesReq(req,res) {
-  const searchQuery = req.query.city;
+  const searchQuery = req.query;
   if (!searchQuery) {
     res.status(500).send('Sorry, something went wrong')
   }
-  const url = `https://api.themoviedb.org/3/search/movie?`
+  const url = `https://api.themoviedb.org/3/search/movie`
   const movieQueryPara = {
     query: searchQuery,
-    key: MOVIE_API_KEY,
-    // language:
-    // region:
+    api_key: MOVIE_API_KEY,
   }
   superagent.get(url).query(movieQueryPara).then((movieData) => {
-    const newMovie = new Movie(searchQuery, movieData);
+    const moviesList = movieData.results.map((movie) => {
+      new Movie(moviesList.slice(0,21));
+    })
+   
     res.status(200).send(newMovie);
   })
 }
-function Movie(searchQuery,dataMovie) {
-  this.title = dataMovie.results[0].title ;
-  this.overview = dataMovie.results[0].overview;
-  this.average_votes = dataMovie.results[0].average_votes;
-  this.total_votes = dataMovie.results[0].total_votes;
-  this.image_url = dataMovie.results[0].image_url;
-  this.popularity = dataMovie.results[0].popularity;
-  this.released_on = dataMovie.results[0].released_on;
-}
 
 
-function handleYelpReq(req,res) {
-  const searchQuery = req.query.city;
-  if (!searchQuery) {
-    res.status(500).send('Sorry, something went wrong')
-  }
-  const url = `https://api.yelp.com/v3/businesses/search`,
-  const restaurantQueryPara ={
-  categories:'restaurants',
-  location: searchQuery
-  }
-  // API
-  superagent
-  .get(url).query(restaurantQueryPara).set(`Authorization`,`Bearer ${YELP_API_KEY}`).then((yelpData) => {
-    const newYelp = new Yelp(searchQuery, yelpData);
-    res.status(200).send(newYelp);
-  })
-  .catch((error) => {
-    res.status(500).send('Sorry, something went wrong');
-  });
-}
+
+// function handleYelpReq(req,res) {
+//   const searchQuery = req.query.city;
+//   if (!searchQuery) {
+//     res.status(500).send('Sorry, something went wrong')
+//   }
+//   const url = `https://api.yelp.com/v3/businesses/search`,
+//   const restaurantQueryPara ={
+//   categories:'restaurants',
+//   location: searchQuery
+//   }
+//   // API
+//   superagent
+//   .get(url).query(restaurantQueryPara).set(`Authorization`,`Bearer ${YELP_API_KEY}`).then((yelpData) => {
+//     const newYelp = new Yelp(searchQuery, yelpData);
+//     res.status(200).send(newYelp);
+//   })
+//   .catch((error) => {
+//     res.status(500).send('Sorry, something went wrong');
+//   });
+// }
  
-function Yelp(searchQuery,dataYelp) {
-  this.name = ;
-  this.image_url =;
-  this.price =;
-  this.rating = ;
-  this.url = ;
-} 
+// function Yelp(searchQuery,dataYelp) {
+//   this.name = ;
+//   this.image_url =;
+//   this.price =;
+//   this.rating = ;
+//   this.url = ;
+// } 
 
+
+// app listener
 cityApp.listen(PORT, () => console.log(`Listening to Port ${PORT}`));
 
+
+// General/All Error Handler
 function allError(req, res) {
   res.status(500).send('Sorry, something went wrong');
 }
 
-// error handler for all types of errors
 cityApp.use('*', allError);
+
+
+
+
+  
+
 
